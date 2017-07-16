8ca27348261e269b3a85471b7885516aa7e2bef7
Brandon Miller <brandon.thomas.miller@gmail.com>
2017-07-16 14:29:23 -0700

    added us-timezone assignment

---
+++ us-timezone/src/app/app.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
+})
+
+export class AppComponent {
+    title = 'US TIME ZONE DISPLAY';
+    lastTimeZone = null;
+    time = new Date();
+    changeTime(timezone){
+        this.time = new Date();
+        if (timezone === 'MST') {
+            this.time.setHours(this.time.getHours() + 1);
+        } else if (timezone === 'CST') {
+            this.time.setHours(this.time.getHours() + 2);
+        } else if (timezone === 'EST') {
+            this.time.setHours(this.time.getHours() + 3);
+        } else if (timezone === 'PST') {
+            this.time.setHours(this.time.getHours())
+        }
+        this.lastTimeZone = timezone;
+    }
+}
